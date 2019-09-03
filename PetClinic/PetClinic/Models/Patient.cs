4a2f22119552bbd3871e1b5315a1872e3516b926
Gino Mortillero <mortillerogino@gmail.com>
2019-09-03 09:45:47 +0800

    Added Patient Model

---
+++ PetClinic/PetClinic/Models/Patient.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PetClinic.Models
+{
+    public class Patient
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime DateAdded { get; set; }
+
+    }
+}
