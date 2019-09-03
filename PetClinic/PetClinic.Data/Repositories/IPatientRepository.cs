e8208c3f1975e2426f014bfad5fa54c83c04c55e
Gino Mortillero <mortillerogino@gmail.com>
2019-09-03 10:28:44 +0800

    Added IPatientRepository and updated IRepository

---
+++ PetClinic/PetClinic.Data/Repositories/IPatientRepository.cs
@@ -0,0 +1,12 @@
+﻿using PetClinic.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PetClinic.Data.Repositories
+{
+    public interface IPatientRepository : IRepository<Patient>
+    {
+        int GetCount();
+    }
+}
--- PetClinic/PetClinic.Data/Repositories/IRepository.cs
+++ PetClinic/PetClinic.Data/Repositories/IRepository.cs
@@ -1,15 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace PetClinic.Data.Repositories
 {
     public interface IRepository<TEntity> where TEntity : class
     {
-        TEntity GetByName(string name);
-        TEntity GetById(int id);
-        TEntity Update(TEntity updatedEntity);
-        TEntity Add(TEntity entity);
-        TEntity Delete(TEntity entity);
+        List<TEntity> Get(
+        Expression<Func<TEntity, bool>> filter = null,
+        Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+        params Expression<Func<TEntity, object>>[] includes);
+
+        IQueryable<TEntity> Query(
+        Expression<Func<TEntity, bool>> filter = null,
+        Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null);
+
+        TEntity GetById(object id);
+
+        TEntity GetFirstOrDefault(
+        Expression<Func<TEntity, bool>> filter = null,
+        params Expression<Func<TEntity, object>>[] includes);
+
+        void Insert(TEntity entity);
+
+        void Update(TEntity entity);
+
+        void Delete(object id);
     }
 }
