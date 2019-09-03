dd27a9e33e2b7550d8fd6a655de53f6031e099b7
Gino Mortillero <mortillerogino@gmail.com>
2019-09-03 10:59:21 +0800

    Added IUnitOfWork interface. Added UnitOfWork EF Implementation.

---
+++ PetClinic/PetClinic.Data/Repositories/EntityFramework/UnitOfWork.cs
@@ -0,0 +1,53 @@
+﻿using PetClinic.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetClinic.Data.Repositories.EntityFramework
+{
+    public class UnitOfWork : IUnitOfWork, IDisposable
+    {
+        private readonly PetClinicContext _context;
+        private bool _disposed = false;
+
+        public UnitOfWork(PetClinicContext context)
+        {
+            _context = context;
+        }
+
+        private IPatientRepository _patientRepository;
+        public IPatientRepository PatientsRepository
+        {
+            get
+            {
+                if (_patientRepository == null)
+                {
+                    _patientRepository = new PatientRepository(_context);
+                }
+
+                return _patientRepository;
+            }
+        }
+
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+
+        public async Task<int> CommitAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+
+        public void Dispose()
+        {
+            if (!_disposed)
+            {
+                _context.Dispose();
+            }
+            _disposed = true;
+        }
+    }
+}
---
+++ PetClinic/PetClinic.Data/Repositories/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetClinic.Data.Repositories
+{
+    public interface IUnitOfWork
+    {
+        IPatientRepository PatientsRepository { get; }
+
+        void Commit();
+
+        Task<int> CommitAsync();
+    }
+}
