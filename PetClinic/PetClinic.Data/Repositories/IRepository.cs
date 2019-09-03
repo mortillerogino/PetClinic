abf25ef1cc032ef12385c5a6f55dd5ded85a5a7a
Gino Mortillero <mortillerogino@gmail.com>
2019-09-03 10:01:33 +0800

    Added IRepository Interface

---
+++ PetClinic/PetClinic.Data/Repositories/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PetClinic.Data.Repositories
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        TEntity GetByName(string name);
+        TEntity GetById(int id);
+        TEntity Update(TEntity updatedEntity);
+        TEntity Add(TEntity entity);
+        TEntity Delete(TEntity entity);
+    }
+}
